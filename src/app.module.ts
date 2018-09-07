b3a3f20771196547eb502283c383491508d5053e
jnst <jnst@outlook.com>
2018-09-07 21:49:56 +0900

    Remove unneeded code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import {Module} from '@nestjs/common';
 import {TypeOrmModule} from '@nestjs/typeorm';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
 import {PhotoModule} from './photo/photo.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), PhotoModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
